4c25d8ed7f32f9d190d52eadbfabee1c31d771e4
Phachara Pornanothai <phacharaphruk@gmail.com>
2026-03-03 13:45:10 +0700

    fixed-home-and-something-error

--- frontend_app/src/firebase.js
+++ frontend_app/src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app"
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBHV5IZLmD8M5BTQpOTbXrKyOk4C63EA4A",
-    authDomain: "mlops-auth-c08ab.firebaseapp.com",
-    projectId: "mlops-auth-c08ab",
-    storageBucket: "mlops-auth-c08ab.firebasestorage.app",
-    messagingSenderId: "358558029175",
-    appId: "1:358558029175:web:edf9a5b2de29066e33fd72",
-    measurementId: "G-19M0F8LBEJ"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 }
 
 const app = initializeApp(firebaseConfig)
